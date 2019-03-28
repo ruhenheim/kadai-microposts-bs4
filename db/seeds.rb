2923fc796bbe29a0521b0b1cbbaf6198ad756344
ruhenheim <asd.uchiyama@gmail.com>
2019-03-28 04:23:41 +0000

    初期データ:faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Faker::Config.locale = :ja
+
+# ダミーユーザ用アカウントと、そのテストデータ
+10.times do |c|
+  user = User.create(
+            name: Faker::Name.name,
+            email: Faker::Internet.email,
+            password: "test"
+          )
+  100.times do |m|
+    user.microposts.create(content: Faker::Lorem.sentence)
+  end
+end
